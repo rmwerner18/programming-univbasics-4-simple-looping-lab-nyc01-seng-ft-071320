32e7705aae08e21964c7ee76ffbf908c458de76b
Ryan Werner <rmwerner@umich.edu>
2020-06-15 16:43:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,3 +14,11 @@ def loop_message_n_times(message, int)
     count += 1
   end
 end
+
+def output_array(array)
+  count = 0
+  while array[count] do
+    puts array[count]
+    count +=1
+  end
+end
