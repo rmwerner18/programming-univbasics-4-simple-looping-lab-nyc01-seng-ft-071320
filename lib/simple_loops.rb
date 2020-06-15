c49f047c487a6fe3467cad5997bf4479e5db019a
Ryan Werner <rmwerner@umich.edu>
2020-06-15 16:41:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,11 @@ def loop_message_five_times(message)
     count += 1
   end
 end
+
+def loop_message_n_times(message, int)
+  count = 0 
+  while count < int do
+    puts message
+    count += 1
+  end
+end
